2475b456f5fb04aa0475372764bf72193139033e
PalakPartani <partanipalak16@gmail.com>
2020-08-29 10:46:51 -0700

    [Palak]ADDED:Feature to toss player who will play first

--- TicTacToeProblem/app.js
+++ TicTacToeProblem/app.js
@@ -2,6 +2,7 @@ console.log('Welcome to TicTacToe');
 //Declared array.
 let gameBoard = new Array();
 let playerSign;
+let playerTurn;
 //Resetting Game Board By Initilizing Array With Default Value
 let resetBoard = () => {
     gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -11,11 +12,13 @@ let resetBoard = () => {
 let assignSignToPlayer = () => {
     if ((Math.random() % 2) == 1) {
         playerSign = 'X';
+        playerTurn = true;
     }
     else {
         playerSign = 'O';
+        playerTurn = true;
     }
-    console.log("The player sign is : " + playerSign);
+    console.log("The player1 sign is : " + playerSign);
 };
 resetBoard();
 assignSignToPlayer();
