9a355a2db1b5c72392078f6c464e5e8e2053b0b0
PalakPartani <partanipalak16@gmail.com>
2020-08-29 10:37:29 -0700

    [Palak]ADDED:Feature to assign symbol 'X' or 'O' to player

--- TicTacToeProblem/app.js
+++ TicTacToeProblem/app.js
@@ -1,10 +1,22 @@
 console.log('Welcome to TicTacToe');
 //Declared array.
 let gameBoard = new Array();
+let playerSign;
 //Resetting Game Board By Initilizing Array With Default Value
 let resetBoard = () => {
     gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     console.log("The board is resetted: " + gameBoard);
 };
+//Assigning Letter X or O To Player 
+let assignSignToPlayer = () => {
+    if ((Math.random() % 2) == 1) {
+        playerSign = 'X';
+    }
+    else {
+        playerSign = 'O';
+    }
+    console.log("The player sign is : " + playerSign);
+};
 resetBoard();
+assignSignToPlayer();
 //# sourceMappingURL=app.js.map
