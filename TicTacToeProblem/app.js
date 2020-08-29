fc8cab0e647cc310dce5abb5b1ce8325c8091caa
PalakPartani <partanipalak16@gmail.com>
2020-08-29 10:25:18 -0700

    [Palak]ADDED:Feature to reset the board

--- TicTacToeProblem/app.js
+++ TicTacToeProblem/app.js
@@ -5,5 +5,6 @@ let gameBoard = new Array();
 let resetBoard = () => {
     gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     console.log("The board is resetted: " + gameBoard);
-}
+};
 resetBoard();
+//# sourceMappingURL=app.js.map
